15c815bfff04d6ab425cde6d160015232287b3d6
touristicmaina <touristicmaina@gmail.com>
2026-01-16 22:23:00 +0300

    Refactor ticket retrieval in TicketviewComponent

--- src/app/views/pages/tickets/ticketview/ticketview.component.ts
+++ src/app/views/pages/tickets/ticketview/ticketview.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { TicketService } from '../../../../services/ticket.service';
 import { Ticket } from '../../../../models/ticket.model';
 
 @Component({
   selector: 'app-ticketview',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './ticketview.component.html',
   styleUrls: ['./ticketview.component.scss']
 })
@@ -19,17 +22,9 @@ export class TicketviewComponent implements OnInit {
 
   ngOnInit(): void {
     const ticketId = this.route.snapshot.paramMap.get('id');
-
     if (ticketId) {
-      this.ticketService.getTicketById(ticketId).subscribe({
-        next: (data: Ticket | undefined) => {
-          if (data) {
-            this.ticket = data;
-          }
-        },
-        error: (err: any) => {
-          console.error('Error loading ticket', err);
-        }
+      this.ticketService.getTicketById(ticketId).subscribe(ticket => {
+        this.ticket = ticket;
       });
     }
   }
