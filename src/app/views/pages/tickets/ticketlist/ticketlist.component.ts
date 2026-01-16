929f3ceb8fe63b417d89104dbdf8d60629b04a27
touristicmaina <touristicmaina@gmail.com>
2026-01-16 22:45:42 +0300

    Implement ticket fetching in TicketlistComponent

--- src/app/views/pages/tickets/ticketlist/ticketlist.component.ts
+++ src/app/views/pages/tickets/ticketlist/ticketlist.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { TicketService } from '../../../../services/ticket.service';
+import { Ticket } from '../../../../models/ticket.model';
 
 @Component({
   selector: 'app-ticketlist',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './ticketlist.component.html',
-  styleUrls: ['./ticketlist.component.scss']
 })
-export class TicketlistComponent {}
+export class TicketlistComponent implements OnInit {
+
+  tickets: Ticket[] = [];
+  filteredTickets: Ticket[] = [];
+
+  constructor(private ticketService: TicketService) {}
+
+  ngOnInit(): void {
+    this.ticketService.getTickets().subscribe(tickets => {
+      this.tickets = tickets;
+      this.filteredTickets = tickets;
+    });
+  }
+}
