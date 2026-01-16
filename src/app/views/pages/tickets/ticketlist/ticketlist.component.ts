70272bc96a79b88f002bda609feb84e136983dc8
touristicmaina <touristicmaina@gmail.com>
2026-01-16 22:22:22 +0300

    Refactor TicketlistComponent to standalone
    Removed ticket loading logic and service dependency.

--- src/app/views/pages/tickets/ticketlist/ticketlist.component.ts
+++ src/app/views/pages/tickets/ticketlist/ticketlist.component.ts
@@ -1,32 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { TicketService } from '../../../../services/ticket.service';
-import { Ticket } from '../../../../models/ticket.model';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ticketlist',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './ticketlist.component.html',
   styleUrls: ['./ticketlist.component.scss']
 })
-export class TicketlistComponent implements OnInit {
-
-  tickets: Ticket[] = [];
-  filteredTickets: Ticket[] = [];
-
-  constructor(private ticketService: TicketService) {}
-
-  ngOnInit(): void {
-    this.loadTickets();
-  }
-
-  loadTickets(): void {
-    this.ticketService.getTickets().subscribe({
-      next: (data: Ticket[]) => {
-        this.tickets = data;
-        this.filteredTickets = data;
-      },
-      error: (err: any) => {
-        console.error(err);
-      }
-    });
-  }
-}
+export class TicketlistComponent {}
