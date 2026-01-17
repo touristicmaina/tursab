5713d105cb8c091dc8c01592588d2f0c62bff334
touristicmaina <touristicmaina@gmail.com>
2026-01-17 04:09:43 +0300

    Update tickets.component.ts

--- src/app/views/pages/tickets/tickets.component.ts
+++ src/app/views/pages/tickets/tickets.component.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
+  imports: [CommonModule],
   templateUrl: './tickets.component.html'
 })
 export class TicketsComponent {
-
-  formValue = {
-    restAmount: 0
-  };
-
 }
