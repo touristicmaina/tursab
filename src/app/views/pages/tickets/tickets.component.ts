65d476695d1a3a3101431256633d7bfea9ef1e41
touristicmaina <touristicmaina@gmail.com>
2026-01-17 03:02:33 +0300

    Update tickets.component.ts

--- src/app/views/pages/tickets/tickets.component.ts
+++ src/app/views/pages/tickets/tickets.component.ts
@@ -1,51 +1,19 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Ticket } from 'src/app/models/ticket.model';
+import { Ticket } from '../../../models/ticket.model';
+import { ClientsService } from '../../../services/clients.service';
+import { ServicesListService } from '../../../services/serviceslist.service';
 
 @Component({
   selector: 'app-tickets',
-  standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './tickets.component.html',
+  templateUrl: './tickets.component.html'
 })
 export class TicketsComponent {
 
+  ticket: Partial<Ticket> = {};
   mode: 'add' | 'edit' = 'add';
 
-  formValue: any = {
-    client: {
-      id: '',
-      name: '',
-      phone: '',
-      hotel: '',
-      pax: 1
-    },
-    activity: {
-      id: '',
-      name: ''
-    },
-    salePrice: 0,
-    currency: 'USD',
-    paymentStatus: 'PAID',
-    restAmount: 0
-  };
-
-  saveTicket() {
-
-    const ticket: Partial<Ticket> = {
-      client: this.formValue.client,
-      activity: this.formValue.activity,
-      salePrice: {
-        amount: this.formValue.salePrice,
-        currency: this.formValue.currency
-      },
-      paymentStatus: this.formValue.paymentStatus,
-      rest: this.formValue.paymentStatus === 'PAID'
-        ? { amount: 0, currency: this.formValue.currency }
-        : { amount: this.formValue.restAmount, currency: this.formValue.currency }
-    };
-
-    console.log('TICKET:', ticket);
-  }
+  constructor(
+    private clientService: ClientsService,
+    private activityService: ServicesListService
+  ) {}
 }
