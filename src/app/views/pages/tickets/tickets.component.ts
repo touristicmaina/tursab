a3119a26a191df6c618d8029f4d821cf89e4d457
touristicmaina <touristicmaina@gmail.com>
2026-01-17 04:33:44 +0300

    Update tickets.component.ts

--- src/app/views/pages/tickets/tickets.component.ts
+++ src/app/views/pages/tickets/tickets.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule   // 👈 ضروري لـ ngModel
+  ],
   templateUrl: './tickets.component.html'
 })
 export class TicketsComponent {
+
+  // 👇 لازم يكون موجود لأنو مستخدم بالـ HTML
+  formValue = {
+    restAmount: 0
+  };
+
 }
