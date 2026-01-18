831c60d4025d2284dd9d9553dc2adb60b7e92548
touristicmaina <touristicmaina@gmail.com>
2026-01-18 03:17:49 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
-// src/environments/environment.ts
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCRhTTbmxnFMQaw6PuL3mKI1bx6La_bUh4",
+    apiKey: "AIzaSyCRnTTbmxnFMQaw6PuL3mK1bX6La_bUh4",
     authDomain: "tursab-ad4c8.firebaseapp.com",
     projectId: "tursab-ad4c8",
-    storageBucket: "tursab-ad4c8.firebasestorage.app",
+    storageBucket: "tursab-ad4c8.appspot.com",
     messagingSenderId: "465347343902",
-    appId: "1:465347343902:web:7074127e76b232bfc6e00f",
+    appId: "1:465347343902:web:7074127e76b232bcf6ee0f",
     measurementId: "G-L00XVY2HXW"
   }
 };
