bf65bfae760f1b93c8dfba559404609a74fc475e
touristicmaina <touristicmaina@gmail.com>
2026-01-18 21:02:21 +0300

    Update auth.guard.ts

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,19 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { map } from 'rxjs';
-import { AuthService } from './auth.service';
 
 export const AuthGuard: CanActivateFn = () => {
-  const authService = inject(AuthService);
   const router = inject(Router);
 
-  return authService.user$.pipe(
-    map(user => {
-      if (user) {
-        return true;
-      }
-      router.navigate(['/login']);
-      return false;
-    })
-  );
+  const isLoggedIn = localStorage.getItem('loggedIn');
+
+  if (isLoggedIn) {
+    return true;
+  } else {
+    router.navigate(['/login']);
+    return false;
+  }
 };
