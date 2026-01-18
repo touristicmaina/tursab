a6da5dc23dc82e04013bf2f72f78c1d6493cbc1a
touristicmaina <touristicmaina@gmail.com>
2026-01-18 17:07:58 +0300

    Update auth.guard.ts

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,19 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { map } from 'rxjs/operators';
-import { AuthService } from '../app/services/auth.service';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthGuard implements CanActivate {
-  constructor(private auth: AuthService, private router: Router) {}
 
-  canActivate() {
-    return this.auth.user$.pipe(
-      map(user => {
-        if (user) return true;
-        this.router.navigate(['/login']);
-        return false;
-      })
-    );
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    return true; // ⬅️ فتح كل الصفحات مؤقتاً
   }
 }
