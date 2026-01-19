4799e32291c8a1bdf37181d96d5e6da090d3681b
touristicmaina <touristicmaina@gmail.com>
2026-01-20 02:42:36 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,7 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
-
-// Layout
 import { DefaultLayoutComponent } from './layout/default-layout/default-layout.component';
-
-// Pages
 import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { AddClientComponent } from './views/pages/add-client/add-client.component';
-import { ClientsListComponent } from './views/pages/clients-list/clients-list.component';
-import { ServicesComponent } from './views/pages/services/services.component';
-import { AddTicketComponent } from './views/pages/add-ticket/add-ticket.component';
-import { TicketsComponent } from './views/pages/tickets/tickets.component';
 
 export const routes: Routes = [
   {
@@ -19,11 +10,31 @@ export const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: '', component: DashboardComponent },
-      { path: 'add-client', component: AddClientComponent },
-      { path: 'clients', component: ClientsListComponent },
-      { path: 'services', component: ServicesComponent },
-      { path: 'add-ticket', component: AddTicketComponent },
-      { path: 'tickets', component: TicketsComponent }
-    ]
-  }
+
+      {
+        path: 'clients',
+        loadComponent: () =>
+          import('./views/pages/clients/clients.component')
+            .then(m => m.ClientsComponent),
+      },
+
+      {
+        path: 'tickets',
+        loadComponent: () =>
+          import('./views/pages/tickets/tickets.component')
+            .then(m => m.TicketsComponent),
+      },
+
+      {
+        path: 'activities',
+        loadComponent: () =>
+          import('./views/pages/activities/activities.component')
+            .then(m => m.ActivitiesComponent),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
