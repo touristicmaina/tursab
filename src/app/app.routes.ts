ebad2881962ddb2a0cd42b6314d199d545882761
touristicmaina <touristicmaina@gmail.com>
2026-01-20 02:51:45 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,40 +1,58 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
-import { DefaultLayoutComponent } from './layout/default-layout/default-layout.component';
-import { DashboardComponent } from './views/dashboard/dashboard.component';
 
+// Layout
+import { DefaultLayoutComponent } from './layout/default-layout/default-layout.component';
+
+// Pages (Lazy Loaded – Standalone)
 export const routes: Routes = [
   {
     path: '',
     component: DefaultLayoutComponent,
     canActivate: [AuthGuard],
     children: [
-      { path: '', component: DashboardComponent },
-
+      {
+        path: '',
+        loadComponent: () =>
+          import('./views/dashboard/dashboard.component')
+            .then(m => m.DashboardComponent),
+      },
+      {
+        path: 'activities',
+        loadComponent: () =>
+          import('./views/pages/activities/activities/activities.component')
+            .then(m => m.ActivitiesComponent),
+      },
       {
         path: 'clients',
         loadComponent: () =>
-          import('./views/pages/clients/clients.component')
-            .then(m => m.ClientsComponent),
+          import('./views/pages/clients/clients-list/clients-list.component')
+            .then(m => m.ClientsListComponent),
       },
-
+      {
+        path: 'add-client',
+        loadComponent: () =>
+          import('./views/pages/add-client/add-client.component')
+            .then(m => m.AddClientComponent),
+      },
+      {
+        path: 'services',
+        loadComponent: () =>
+          import('./views/pages/services/services.component')
+            .then(m => m.ServicesComponent),
+      },
       {
         path: 'tickets',
         loadComponent: () =>
           import('./views/pages/tickets/tickets.component')
             .then(m => m.TicketsComponent),
       },
-
       {
-        path: 'activities',
+        path: 'add-ticket',
         loadComponent: () =>
-          import('./views/pages/activities/activities.component')
-            .then(m => m.ActivitiesComponent),
+          import('./views/pages/add-ticket/add-ticket.component')
+            .then(m => m.AddTicketComponent),
       },
     ],
-  },
-  {
-    path: '**',
-    redirectTo: '',
   },
 ];
