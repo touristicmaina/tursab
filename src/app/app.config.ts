01b5758f63396855eae9a77a8656a5766f64bf92
touristicmaina <touristicmaina@gmail.com>
2026-01-20 04:36:51 +0300

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
+import { routes } from './views/pages/routes';
 
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-
-export const appConfig: ApplicationConfig = {
+export const appConfig = {
   providers: [
-    provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(environment.firebase))
+    provideRouter(routes)
   ]
 };
