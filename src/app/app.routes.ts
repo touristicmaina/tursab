6b432db84f705656951f9b1f3d9ee3bbc0d79ffb
touristicmaina <touristicmaina@gmail.com>
2026-01-20 04:23:46 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,68 +5,60 @@ import { LoginComponent } from './views/pages/login/login.component';
 import { RegisterComponent } from './views/pages/register/register.component';
 import { ResetPasswordComponent } from './views/pages/reset-password/reset-password.component';
 
-/* Pages */
+/* Errors */
 import { Page404Component } from './views/pages/page404/page404.component';
 import { Page500Component } from './views/pages/page500/page500.component';
 
 /* Activities */
 import { ActivitiesComponent } from './views/pages/activities/activities.component';
-import { ActivitiesListComponent } from './views/pages/activities/activitieslist/activitieslist.component';
-import { ActivityAnalysisComponent } from './views/pages/activities/activityanalysis/activityanalysis.component';
+import { ActivitieslistComponent } from './views/pages/activities/activitieslist/activitieslist.component';
+import { ActivityanalysisComponent } from './views/pages/activities/activityanalysis/activityanalysis.component';
 
 /* Clients */
 import { ClientsComponent } from './views/pages/clients/clients.component';
-import { ClientsListComponent } from './views/pages/clients/clients-lists/clientslist/clientslist.component';
-import { CreateClientComponent } from './views/pages/clients/create-clients/createclient/createclient.component';
+import { ClientslistComponent } from './views/pages/clients/clients-lists/clientslist/clientslist.component';
+import { CreateclientComponent } from './views/pages/clients/create-clients/createclient/createclient.component';
 
 /* Tickets */
 import { TicketsComponent } from './views/pages/tickets/tickets.component';
-import { TicketListComponent } from './views/pages/tickets/ticketlist/ticketlist.component';
-import { TicketViewComponent } from './views/pages/tickets/ticketview/ticketview.component';
+import { TicketlistComponent } from './views/pages/tickets/ticketlist/ticketlist.component';
+import { TicketviewComponent } from './views/pages/tickets/ticketview/ticketview.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
 
-  /* Auth */
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
 
-  /* Activities */
   {
     path: 'activities',
     component: ActivitiesComponent,
     children: [
-      { path: '', redirectTo: 'list', pathMatch: 'full' },
-      { path: 'list', component: ActivitiesListComponent },
-      { path: 'analysis', component: ActivityAnalysisComponent }
+      { path: '', component: ActivitieslistComponent },
+      { path: 'analysis', component: ActivityanalysisComponent }
     ]
   },
 
-  /* Clients */
   {
     path: 'clients',
     component: ClientsComponent,
     children: [
-      { path: '', redirectTo: 'list', pathMatch: 'full' },
-      { path: 'list', component: ClientsListComponent },
-      { path: 'create', component: CreateClientComponent }
+      { path: '', component: ClientslistComponent },
+      { path: 'create', component: CreateclientComponent }
     ]
   },
 
-  /* Tickets */
   {
     path: 'tickets',
     component: TicketsComponent,
     children: [
-      { path: '', redirectTo: 'list', pathMatch: 'full' },
-      { path: 'list', component: TicketListComponent },
-      { path: 'view', component: TicketViewComponent }
+      { path: '', component: TicketlistComponent },
+      { path: ':id', component: TicketviewComponent }
     ]
   },
 
-  /* Errors */
   { path: '500', component: Page500Component },
   { path: '404', component: Page404Component },
   { path: '**', redirectTo: '404' }
+
 ];
