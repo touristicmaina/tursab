860af71e84b6de6cf83fc9b9c19d7d237eb6caab
touristicmaina <touristicmaina@gmail.com>
2026-01-20 04:43:22 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+/* Auth */
+import { LoginComponent } from './views/pages/login/login.component';
+import { RegisterComponent } from './views/pages/register/register.component';
+import { ResetPasswordComponent } from './views/pages/reset-password/reset-password.component';
+
+/* Pages */
+import { Page404Component } from './views/pages/page404/page404.component';
+import { Page500Component } from './views/pages/page500/page500.component';
+
+/* Activities */
+import { ActivitieslistComponent } from './views/pages/activities/activitieslist/activitieslist.component';
+import { ActivityanalysisComponent } from './views/pages/activities/activityanalysis/activityanalysis.component';
+
+/* Clients */
+import { ClientsComponent } from './views/pages/clients/clients.component';
+import { ClientslistComponent } from './views/pages/clients/clients-lists/clientslist/clientslist.component';
+
+/* Tickets */
+import { TicketlistComponent } from './views/pages/tickets/ticketlist/ticketlist.component';
+import { TicketviewComponent } from './views/pages/tickets/ticketview/ticketview.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  /* Auth */
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
+
+  /* Activities */
+  { path: 'activities/list', component: ActivitieslistComponent },
+  { path: 'activities/analysis', component: ActivityanalysisComponent },
+
+  /* Clients */
+  { path: 'clients', component: ClientsComponent },
+  { path: 'clients/list', component: ClientslistComponent },
+
+  /* Tickets */
+  { path: 'tickets/list', component: TicketlistComponent },
+  { path: 'tickets/view', component: TicketviewComponent },
+
+  /* Errors */
+  { path: '500', component: Page500Component },
+  { path: '404', component: Page404Component },
+  { path: '**', redirectTo: '404' }
+];
